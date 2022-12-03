87d5dacbda076ebea7eca4cd5b49404585cf3dcd
codeyourwaytofreedom <can.kolej@gmail.com>
2022-12-03 16:01:33 +0300

    Navbar improved. Css updated.
    Bids animation to be added..
    
    Keep going.
    
    Remember why you started.

--- src/navbar/navbar.js
+++ src/navbar/navbar.js
@@ -1,5 +1,10 @@
 import "./navbar.css";
 import auction from "./auction.png";
+import login from "./login.png"
+import signup from "./signup.png"
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlusCircle, faSignIn } from '@fortawesome/free-solid-svg-icons';
 
 const Navbar = () => {
     return ( 
@@ -9,6 +14,24 @@ const Navbar = () => {
             </div>
             <div className="Navbar_motto">
                     auctioneer
+            </div>
+            <div className="Navbar_member">
+                <div className="Navbar_member_login">
+                    <button>
+                        <div className="Navbar_member_login_icon">
+                            <FontAwesomeIcon icon={faSignIn} size={"2x"}/>
+                        </div>
+                        <span>Sign in</span>
+                    </button>
+                </div>
+                <div className="Navbar_member_signup">
+                    <button>
+                        <div className="Navbar_member_signup_icon">
+                            <FontAwesomeIcon icon={faPlusCircle} size={"2x"} />
+                        </div>
+                        <span>Sign up</span>
+                    </button>
+                </div>
             </div>
         </div>
      );
