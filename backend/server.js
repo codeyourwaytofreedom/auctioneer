963374d4794c3a15fdcf656639add93cc3cfc38b
codeyourwaytofreedom <can.kolej@gmail.com>
2022-12-05 11:23:21 +0300

    Connected to Mongodb in backend. Great step forward.
    Next step: create documents and validate login with dummy data.

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,23 @@ const app = express(); //Line 2
 
 const port = process.env.PORT || 9000; //Line 3
 
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://127.0.0.1:27017/members", {
+  useNewUrlParser: true
+});
+
+const connection = mongoose.connection;
+
+
+2
+3
+connection.once("open", function() {
+  console.log("Connection with MongoDB was successful");
+});
+
+
+
 // This displays message that the server running and listening to specified port
 app.listen(port, () => console.log(`Listening on port ${port}`)); //Line 6
 
