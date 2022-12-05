42953cea94f3cc2389404f59f3dc53a5e712f8bd
codeyourwaytofreedom <can.kolej@gmail.com>
2022-12-05 14:40:02 +0300

    Database connection re-created with MongoClient. Works perfectly. Retrieved data from Mongodb in Node backend.
    Next step: send form inputs to backend with post request.
    
    Keep going.
    
    Remember why you started.

---
+++ backend/db.js
@@ -0,0 +1,23 @@
+
+
+const { MongoClient } = require("mongodb");
+
+
+
+let dbConnection;
+
+
+
+module.exports = {
+    connectToDb: (do_after_connection) => {
+        MongoClient.connect('mongodb://127.0.0.1:27017/members')
+            .then((client)=> {
+                dbConnection = client.db()
+                return do_after_connection()
+            }).catch(err => {
+                console.log(err)
+                return do_after_connection(err)
+            })
+    },
+    getDb: () => dbConnection
+}
--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const app = express(); //Line 2
 
 const port = process.env.PORT || 9000; //Line 3
 
-const mongoose = require("mongoose");
+/* const mongoose = require("mongoose");
 
 mongoose.connect("mongodb://127.0.0.1:27017/members", {
   useNewUrlParser: true
@@ -11,19 +11,31 @@ mongoose.connect("mongodb://127.0.0.1:27017/members", {
 
 const connection = mongoose.connection;
 
-
-2
-3
 connection.once("open", function() {
   console.log("Connection with MongoDB was successful");
-});
+}); */
 
 
 
-// This displays message that the server running and listening to specified port
-app.listen(port, () => console.log(`Listening on port ${port}`)); //Line 6
+let db;
+
+const { connectToDb, getDb } = require("./db");
+connectToDb( (err) => {
+        if(!err){
+          // This displays message that the server running and listening to specified port
+          app.listen(port, () => {console.log(`Listening on port ${port}`)}); //Line 6
+          db = getDb();
+        }
+})
+
+
 
 // create a GET route
 app.get('/express_backend', (req, res) => { //Line 9
-  res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' }); //Line 10
+  let results = [];
+   db.collection('auctioneer').find()
+        .forEach (auc => results.push(auc))
+        .then(() => console.log(results))
+
+   res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' }); //Line 10
 }); //Line 11
