88c57d688022f288e768514c4dc243c4ed1649f9
codeyourwaytofreedom <can.kolej@gmail.com>
2022-12-18 21:05:50 +0300

    Auction page images design done. Auction page bokk your seat section created. Made responsive.
    Keep going.
    
    Remember why you started.

--- src/auction/auction.js
+++ src/auction/auction.js
@@ -1,6 +1,7 @@
 import big from "./205.jpg";
 import small from "./suv.jpg";
 import loading from "./loading.jpg";
+import spotlight from "./spotlight.svg";
 
 import "./auction.css";
 import { useParams } from "react-router-dom";
@@ -12,6 +13,7 @@ const Auction = () => {
     const {id} = useParams();
     const [response_image, setImage] = useState(loading)
     const [response_array, setArray] = useState();
+    const [bigone, setBig] = useState();
 
     useEffect(() => {
         /* const url = "http://localhost:9000/item";
@@ -48,23 +50,23 @@ const Auction = () => {
                             <button id="button-two">2</button>
                             <button id="button-three">3</button>
                         </div>
-                        <div className="auction_item_row_images_smalls_small">
+                        <div className="auction_item_row_images_smalls_small" onClick={()=> setBig(response_array[1])}>
                                 <img src={response_array ? response_array[1] : response_image} alt="" />
                         </div>
-                        <div className="auction_item_row_images_smalls_small">
+                        <div className="auction_item_row_images_smalls_small" onClick={()=> setBig(response_array[2])}>
                                 <img src={response_array ? response_array[2] : response_image} alt="" />
                         </div>
-                        <div className="auction_item_row_images_smalls_small">
+                        <div className="auction_item_row_images_smalls_small" onClick={()=> setBig(response_array[3])}>
                                 <img src={response_array ? response_array[3] : response_image} alt="" />
                         </div>
                     </div>
                     <div className="auction_item_row_images_big">
-                        <img src={response_array ? response_array[0] : response_image} alt="response" />
+                        <img src={bigone ? bigone : response_array ? response_array[1] : response_image} alt="response" />
                     </div>
                 </div>
 
                 <div className="auction_item_row_details">
-                        {[...Array(id.includes('car') ? 15 : 10)].map(e => 
+                        {[...Array(15)].map(e => 
                                 <div className="auction_item_row_details_row">
                                     <span id="title">
                                         Title goes here.
@@ -77,6 +79,48 @@ const Auction = () => {
                 </div>
 
             </div> 
+            <div className="auction_booking">
+                <div className="auction_booking_seats">
+                    <div className="auction_booking_seats_tierone">
+                        <div className="auction_booking_seats_tierone_seat">
+                            <img src={spotlight} alt="" />  
+                        </div>
+                        <div className="auction_booking_seats_tierone_seat">
+                            <img src={spotlight} alt="" />  
+                        </div>
+                        <div className="auction_booking_seats_tierone_seat">
+                            <img src={spotlight} alt="" />  
+                        </div>
+                        <div className="auction_booking_seats_tierone_seat">
+                            <img src={spotlight} alt="" />  
+                        </div>
+                    </div>
+                    <div className="auction_booking_seats_tiertwo">
+                        <div className="auction_booking_seats_tiertwo_seat">
+                            <img src={spotlight} alt="" />  
+                        </div>
+                        <div className="auction_booking_seats_tiertwo_seat">
+                            <img src={spotlight} alt="" />  
+                        </div>
+                        <div className="auction_booking_seats_tiertwo_seat">
+                            <img src={spotlight} alt="" />  
+                        </div>
+                    </div>
+                    <div className="auction_booking_seats_tierthree">
+                        <div className="auction_booking_seats_tierthree_seat">
+                            <img src={spotlight} alt="" />  
+                        </div>
+                        <div className="auction_booking_seats_tierthree_seat">
+                            <img src={spotlight} alt="" />  
+                        </div>
+                    </div>
+                    <div className="auction_booking_seats_tierfour">
+                        <div className="auction_booking_seats_tierfour_seat">
+                            <img src={spotlight} alt="" />  
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
      );
 }
