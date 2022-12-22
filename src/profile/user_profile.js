d39543e2ad09161f211508c71b1116651eca4eb8
codeyourwaytofreedom <can.kolej@gmail.com>
2022-12-22 20:40:30 +0300

    User profile badge fully responsive.
    User profile detailed improved.
    
    General structure made responsive.
    
    Content to be injected.
    
    Keep going.
    
    Remember why you started.

--- src/profile/user_profile.js
+++ src/profile/user_profile.js
@@ -1,6 +1,9 @@
 import "./user_profile.css";
 import user from "./user.png";
 import badge from "./badge.jpg";
+import history from "./history.png";
+import edit from "./edit.png";
+import booked from "./booked.png";
 
 const User_profile = () => {
     return ( 
@@ -25,7 +28,27 @@ const User_profile = () => {
         </div>
         <div className="user_profile_details">
                 <div className="user_profile_details_shell">
-                        Details shell
+                        <div className="user_profile_details_shell_history">
+                            <img src={history} alt="history" />
+                            <div className="user_profile_details_shell_history_content">
+                                    Bidding History
+                            </div>
+                        </div>
+                        <div>
+                            Biddings go here
+                        </div>
+                        <div className="user_profile_details_shell_booked">
+                            <div className="user_profile_details_shell_history_content" id="right">
+                                    Auction Bookings
+                            </div>
+                            <img src={booked} alt="booked" />
+                        </div>
+                        <div className="user_profile_details_shell_edit">
+                            <img src={edit} alt="edit" />
+                            <div className="user_profile_details_shell_history_content">
+                                    Edit profile
+                            </div>
+                        </div>
                 </div>            
         </div>
 
